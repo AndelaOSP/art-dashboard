2ca36dc951877360421525e7ce11704c311d15c1
EmaEvidence <emmanuelalabi563@gmail.com>
2018-05-18 13:18:58 +0100

    test(landing-page): write unit test
    - write unit test
    
    [FInishes 157340053]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Container, Header, Image } from 'semantic-ui-react';
 import './App.css';
 
@@ -10,14 +10,16 @@ const App = () => {
           centered 
           src='http://res.cloudinary.com/damc3mj5u/image/upload/v1526571584/logo_uw39tc.png' 
           alt='Andela logo'
+          id="andela-logo"
         />
         <Header className='landing-heading' inverted content='ART' />
-        <Header as='h1' inverted content='Andela Resource Tracker' />
+        <Header as='h1' className='description' inverted content='Andela Resource Tracker' />
         <Button className='google-button' size='large'>
           <Image 
             floated='left' 
             src='http://res.cloudinary.com/damc3mj5u/image/upload/v1526571608/google-logo_jjjjqs.svg'
-            alt='Google logo'  
+            alt='Google logo'
+            id='google-logo'
           />
           Sign in with Google
         </Button>
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import expect from 'expect';
+
 import App from './App';
 
+describe('Renders <App /> correctly', () => {  
+  const wrapper = shallow(<App />);
+  const andelaLogo = 'http://res.cloudinary.com/damc3mj5u/image/upload/v1526571584/logo_uw39tc.png';
+  const googleLogo = 'http://res.cloudinary.com/damc3mj5u/image/upload/v1526571608/google-logo_jjjjqs.svg';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  expect(div).toMatchSnapshot();
-  ReactDOM.unmountComponentAtNode(div);
+  it('renders page Container', () => {
+    expect(wrapper.find('Container').length).toBe(1);
+  });
+
+  it('renders Google login button', () => {
+    expect(wrapper.find('Button').length).toBe(1);
+    expect(wrapper.find('#google-logo').prop('src')).toEqual(googleLogo);
+  });
+
+  it('renders Andela logo', () => {
+    expect(wrapper.find('#andela-logo').prop('src')).toEqual(andelaLogo);
+  });
+
+  it('renders page title', () => {
+    expect(wrapper.find('.landing-heading').length).toBe(1);
+    expect(wrapper.find('.landing-heading').prop('content')).toEqual('ART');
+  });
+
+  it('renders page description', () => {
+    expect(wrapper.find('.description').length).toBe(1);
+    expect(wrapper.find('.description').prop('content')).toEqual('Andela Resource Tracker');
+  });
+
 });
