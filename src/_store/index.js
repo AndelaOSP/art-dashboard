9133075c1166d0bfed477136c04c64b34ce8bdc5
rotimi-babalola <rotimi.babalola@andela.com>
2018-05-18 12:57:15 +0100

    feat(setup redux): envify store creation in store/index.js

--- src/_store/index.js
+++ src/_store/index.js
@@ -2,14 +2,27 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../_reducers';
+import logger from 'redux-logger';
 
 const middleware = [thunk];
 
-const createStoreWithMiddleware = compose(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__
-    ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-)(createStore);
+const useReduxDevTools = () => {
+  return window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f;
+}
+
+let createStoreWithMiddleware;
+
+if (process.env.NODE_ENV === 'production') {
+  createStoreWithMiddleware = compose(
+    applyMiddleware(...middleware)
+  )(createStore);
+} else {
+  createStoreWithMiddleware = compose(
+    applyMiddleware(...middleware, logger),
+    useReduxDevTools()
+  )(createStore);
+}
 
 const store = createStoreWithMiddleware(rootReducer);
 
