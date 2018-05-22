d7570ae652e2b0eac159c836078b3335ca1fc281
aimeedyki <akuwudike.a@gmail.com>
2018-05-22 16:13:27 +0100

    feat(authentication): change warning message display  to a toast
    [Finishes #157340054]

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Button, Container, Header, Image, Message } from 'semantic-ui-react';
+import { Button, Container, Header, Image } from 'semantic-ui-react';
+import { SemanticToastContainer, toast } from 'react-semantic-toasts';
 
 import { loginAction } from '../_actions/login.action';
 import { auth, firebase } from '../firebase';
@@ -11,10 +12,6 @@ import '../_css/LoginComponent.css';
 const provider = new firebase.auth.GoogleAuthProvider();
 
 class LoginComponent extends React.Component {
-  state = {
-    isVisible: false,
-    message: ''
-  };
 
   redirectToDashboard(props) {
     if (props.isAuthenticated) {
@@ -30,65 +27,65 @@ class LoginComponent extends React.Component {
     this.redirectToDashboard(nextProps);
   }
 
-  // closes the message component
-  handleDismiss = () => {
-    this.setState({
-      isVisible: false,
-      message: ''
-    })
+  // displays error messages in a toast
+  handleErrorMessage = (message) => {
+    const messageTimeout = 5000;
+      toast(
+        {
+          type: 'warning',
+          description: message,
+          time: messageTimeout
+        });
+  }
+
+  // checks that the user used an Andela email
+  validateUser = (result) => {
+    const validAndelaEmail = /^[\w.-]+@andela\.com$/;
+
+    if (validAndelaEmail.test(result.user.email)) {
+      this.props.loginAction(result.credential.accessToken);
+    } else {
+      const message = 'please sign in with your andela email';
+      this.handleErrorMessage(message);
+    }
   }
 
   // authenticates user
   handleLogin = () => {
     auth.signInWithEmail(provider).then(result => {
-      const pattern = /^[\w.-]+@andela\.com$/;
-
-      if (pattern.test(result.user.email)) {
-        this.props.loginAction(result.credential.accessToken);
-      } else {
-        this.setState({
-          isVisible: true,
-          message: 'please sign in with your andela email'
-        });
-      }
+      this.validateUser(result);
     }).catch(error => {
       if (error) {
-        this.setState({
-          isVisible: true,
-          message: error.message
-        });
+        this.handleErrorMessage(error.message);
       }
     });
   }
 
   render() {
     return (
-      <div className='app landing-overlay background'>
-        <Container>
-          <Image
-            centered
-            src='http://res.cloudinary.com/damc3mj5u/image/upload/v1526571584/logo_uw39tc.png'
-            alt='Andela logo'
-            id="andela-logo"
-          />
-          <Header className='landing-heading' inverted content='ART' />
-          <Header as='h1' className='description' inverted content='Andela Resource Tracker' />
-          <Button onClick={this.handleLogin} className='google-button' size='large'>
+      <div>
+        <SemanticToastContainer position="top-right" />
+        <div className='app landing-overlay background'>
+          <Container>
             <Image
-              floated='left'
-              src='http://res.cloudinary.com/damc3mj5u/image/upload/v1526571608/google-logo_jjjjqs.svg'
-              alt='Google logo'
-              id='google-logo'
+              centered
+              src='http://res.cloudinary.com/damc3mj5u/image/upload/v1526571584/logo_uw39tc.png'
+              alt='Andela logo'
+              id="andela-logo"
             />
-            Sign in with Google
+            <Header className='landing-heading' inverted content='ART' />
+            <Header as='h1' className='description' inverted content='Andela Resource Tracker' />
+            <Button onClick={this.handleLogin} className='google-button' size='large'>
+              <Image
+                floated='left'
+                src='http://res.cloudinary.com/damc3mj5u/image/upload/v1526571608/google-logo_jjjjqs.svg'
+                alt='Google logo'
+                id='google-logo'
+              />
+              Sign in with Google
           </Button>
-        </Container>
-        {this.state.isVisible &&
-          <Message
-            warning
-            onDismiss={this.handleDismiss}
-            content={this.state.message}
-          />}
+          </Container>
+        </div>
       </div>
     )
   }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: 'art-dashboard.firebaseapp.com',
-  databaseURL: 'art-dashboard.firebaseio.com',
-  projectId: 'art-dashboard',
-  storageBucket: 'art-dashboard.appspot.com',
-  messagingSenderId: '301823215408',
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 if (!firebase.apps.length) {
