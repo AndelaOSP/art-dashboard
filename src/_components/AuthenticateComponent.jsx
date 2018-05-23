e73593c874f971b18045373048b694374987514f
hector101 <hector1014really32@gmail.com>
2018-05-23 23:29:11 +0100

    feat(fix routing issue) refactor app routing
    - route users in action creator after user signs in successfully.
    
    [(Finishes [#157804224]

--- src/_components/AuthenticateComponent.jsx
+++ src/_components/AuthenticateComponent.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const AuthenticateComponent = ({ component: Component, isAuthenticated, ...options }) => (
-  <Route {...options} render={props => (
-      isAuthenticated
-          ? <Component {...props} />
-          : <Redirect to='/login' />
-  )} />
-)
+const AuthenticateComponent = ({ component: Component, ...options }) => {
+  const token = localStorage.getItem('token');
+  return (
+    <Route {...options} render={props => (
+        token
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+    )} />
+  )
+};
 
 export default AuthenticateComponent;
