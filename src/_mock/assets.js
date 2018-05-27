1a3cc95be5ef781cba2c3c1699894b32b3ffa3f4
hector101 <hector1014really32@gmail.com>
2018-05-27 14:50:06 +0100

    feat(view all assets): write test - add test cases
    (Finishes)[#157678811]

---
+++ src/_mock/assets.js
@@ -0,0 +1,18 @@
+export const assets = [
+  { 
+    category: "Accessories",
+    sub_category: "Computer Accessories",
+    asset_type: "Headset",
+    asset_code: "AND/HS/0077",
+    model_number: "Microsoft Lifechat LX-6000",
+    make: "Microsoft",
+  },
+  { 
+    category: "Accessories",
+    sub_category: "Computer Accessories",
+    asset_type: "Extension Cord",
+    asset_code: "AND/HS/0079",
+    model_number: "Microsoft Lifechat LX-6009",
+    make: "Zealot",
+  }
+];
