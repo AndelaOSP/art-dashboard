86c54a8404e4f0077b91e40e6438a79ca28e9c3e
hector101 <hector1014really32@gmail.com>
2018-05-28 15:14:59 +0100

    bug(fix routing issue) refactor to integrate firebase
    - route user to dashboard after successful autentication
    
    (Finishes)[#157804224]

--- src/_components/AuthenticateComponent.jsx
+++ src/_components/AuthenticateComponent.jsx
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 
 const AuthenticateComponent = ({ component: Component, ...options }) => (
   <Route {...options} render={props => (
-      localStorage.getItem('andela_notification_values_tokens_1576::1454404601')
+      localStorage.getItem('andela_notification_values_tokens_1576::145440460')
           ? <Component {...props} />
           : <Redirect to='/login' />
   )} />
