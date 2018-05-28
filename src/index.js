4f10e000443ba6651e5a1102fc253181c38fd0a8
Thuku <thuku.bonnie@gmail.com>
2018-05-28 15:14:20 +0300

    feat(authorization) add token in outgoing requests
      - create global axios instance with authorization token
    
     [Delivers #157340057]

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import axios from 'axios';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
+import getToken from './_utils/getToken';
+
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.headers.common['Authorization'] = getToken();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
