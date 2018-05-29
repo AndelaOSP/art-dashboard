190c4298939f8110981d0f170d6477004412b262
HawiCaesar <brian.odhiambo932@gmail.com>
2018-05-29 13:39:55 +0300

    feat(category-actions): add return word to axios in category actions

--- src/_actions/category.actions.js
+++ src/_actions/category.actions.js
@@ -5,7 +5,7 @@ const { LOAD_CATEGORIES_SUCCESS } = constants;
 
 export const loadCategories = () => {
   return ((dispatch) => {
-    axios.get('https://my-json-server.typicode.com/HawiCaesar/jsonplaceholders-demo/categories').then((response) => {
+    return axios.get('https://my-json-server.typicode.com/HawiCaesar/jsonplaceholders-demo/categories').then((response) => {
       dispatch({
         type: LOAD_CATEGORIES_SUCCESS,
         payload: response.data,
