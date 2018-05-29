8343dbc7fac5ae2d26841952631b70ef78eb8b49
HawiCaesar <brian.odhiambo932@gmail.com>
2018-05-29 16:40:14 +0300

    feat(actions): comments, remove unnecesary payload - add comments to asset actions - remove unnecessary payload for asset.actions and its tests

--- src/_actions/assetMakes.actions.js
+++ src/_actions/assetMakes.actions.js
@@ -1,17 +1,31 @@
 import axios from 'axios';
 import constants from '../_constants';
 
+// constants
 const { LOAD_ASSET_MAKES_SUCCESS } = constants;
 
+/**
+ * load asset makes thunk
+ *
+ * @return {type} loadAssetMakesSuccess type and payload
+ */
 export const loadAssetMakes = () => {
   return ((dispatch) => {
-    axios.get('https://my-json-server.typicode.com/HawiCaesar/jsonplaceholders-demo/makes').then((response) => {
-      dispatch({
-        type: LOAD_ASSET_MAKES_SUCCESS,
-        payload: response.data,
-      });
+    return axios.get('https://my-json-server.typicode.com/HawiCaesar/jsonplaceholders-demo/makes').then((response) => {
+      dispatch(loadAssetMakesSuccess(response.data));
     }).catch((error) => {
       console.log(error);
     });
   });
+}
+
+/**
+ * loadAssetMakesSuccess action creator
+ *
+ * @param {type} assetMakes list of asset makes
+ *
+ * @return {type} type and payload
+ */
+export const loadAssetMakesSuccess = (assetMakes) => {
+  return { type: LOAD_ASSET_MAKES_SUCCESS, payload: assetMakes };
 }
