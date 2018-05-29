2046a728a49bb59838bd13518508953c1c0f6d26
HawiCaesar <brian.odhiambo932@gmail.com>
2018-05-29 15:23:15 +0300

    feat(post-asset): add action test for posting asset

---
+++ src/_mock/asset.js
@@ -0,0 +1,25 @@
+export default {
+  "id": 1,
+  "asset_code": "AND/HS/0909",
+  "serial_number": "897832SWWS",
+  "model_number": 6,
+  "checkin_status": "string",
+  "assigned_to": {
+    "id": 4,
+    "first_name": "John",
+    "last_name": "Kamau",
+    "full_name": "string",
+    "email": "joekamau@andela.com",
+    "cohort": 0,
+    "slack_handle": "jkamau",
+    "picture": "string",
+    "phone_number": "0701000100",
+    "last_modified": "2018-05-28T10:28:13Z",
+    "date_joined": "2018-05-28T10:28:13Z",
+    "last_login": "2018-05-28T10:28:13Z"
+  },
+  "created_at": "2018-05-28T10:28:13Z",
+  "last_modified": "2018-05-28T10:28:13Z",
+  "current_status": "Available",
+  "asset_type": "Headsets"
+}
