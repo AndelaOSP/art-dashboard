89af6671ba2b5c2df18a5e3dcc315cd9c79aed1a
hector101 <hector1014really32@gmail.com>
2018-05-30 13:41:52 +0100

    bug(fix route) - rename token key name
    [Finishes](#157804224)

--- src/_components/AuthenticateComponent.jsx
+++ src/_components/AuthenticateComponent.jsx
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 
 const AuthenticateComponent = ({ component: Component, ...options }) => (
   <Route {...options} render={props => (
-      localStorage.getItem('andela_notification_values_tokens_1576::145440460')
+      localStorage.getItem('token')
           ? <Component {...props} />
           : <Redirect to='/' />
   )} />
