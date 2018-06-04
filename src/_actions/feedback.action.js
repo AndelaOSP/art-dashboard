b88f68fe1de53843d9b6571bc2994e9b8b35a87f
Stacey <sachungo1@gmail.com>
2018-06-04 15:53:50 +0300

    Example of fixed nitpicks

--- src/_actions/feedback.action.js
+++ src/_actions/feedback.action.js
@@ -4,19 +4,19 @@ import axios from 'axios';
 const { LOAD_FEEDBACK_SUCCESS, LOAD_FEEDBACK_FAILURE } = constants;
 
 export const feedbackAction = (page, limit = 10) => {
-    return (dispatch) => {
-      axios.get(`feedback?_page=${page}&_limit=${limit}`)
-      .then((response) => {
-        return dispatch({
-          type: LOAD_FEEDBACK_SUCCESS,
-          payload: response,
-        });
-      })
-      .catch((error) => {
-        return dispatch({
-          type: LOAD_FEEDBACK_FAILURE,
-          payload: error,
-        });
+  return (dispatch) => {
+    axios.get(`feedback?_page=${page}&_limit=${limit}`)
+    .then((response) => {
+      return dispatch({
+        type: LOAD_FEEDBACK_SUCCESS,
+        payload: response,
       });
-    }
-  };
+    })
+    .catch((error) => {
+      return dispatch({
+        type: LOAD_FEEDBACK_FAILURE,
+        payload: error,
+      });
+    });
+  }
+};
