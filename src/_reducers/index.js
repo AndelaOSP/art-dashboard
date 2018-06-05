8e1d4ed5cd3bbf52b5f3894d6af31902554477b0
HawiCaesar <brian.odhiambo@andela.com>
2018-06-05 11:04:37 +0300

    fix(duplicate-import): remove duplicate import in _reducers/index.js

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -8,7 +8,6 @@ import modelNumber from './modelNumber.reducer';
 import assetsReducer from './assets.reducer';
 import viewAssets from './viewAssets.reducer';
 import feedbackReducer from './feedback.reducer';
-import viewAssets from './viewAssets.reducer';
 
 const rootReducer = combineReducers({
   categoriesList: categoryReducer,
@@ -19,7 +18,6 @@ const rootReducer = combineReducers({
   assetsList: assetsReducer,
   viewAssets,
   feedbackReducer,
-  viewAssets,
 });
 
 export default rootReducer;
