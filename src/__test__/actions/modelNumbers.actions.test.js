d72cf7c751bf9b42fcee4d0525e92c7779150c73
HawiCaesar <brian.odhiambo@andela.com>
2018-06-12 21:54:16 +0300

    feat(Proptypes): correct some prop-types, add action, reducer tests

--- src/__test__/actions/modelNumbers.actions.test.js
+++ src/__test__/actions/modelNumbers.actions.test.js
@@ -7,10 +7,17 @@ import thunk from 'redux-thunk';
 
 // constants
 import constants from '../../_constants';
-const { LOAD_ASSET_MODEL_NUMBERS } = constants;
+const {
+  LOAD_ASSET_MODEL_NUMBERS,
+  CREATE_MODEL_NUMBER_SUCCESS,
+  CREATE_MODEL_NUMBER_FAILURE
+} = constants;
 
 // actions
-import { loadModelNumbers } from '../../_actions/modelNumbers.actions';
+import {
+  loadModelNumbers,
+  createModelNumbers
+} from '../../_actions/modelNumbers.actions';
 
 // store
 const middleware = [thunk];
@@ -18,14 +25,24 @@ const mockStore = configureMockStore(middleware);
 let store;
 
 describe('Model Numbers action tests', () => {
-  const mock = new MockAdapter(axios);
+  let mock = new MockAdapter(axios);
   let url = 'https://my-json-server.typicode.com/HawiCaesar/jsonplaceholders-demo/model_numbers';
   store = mockStore({});
   let expectedActions = [
-    {
-      type: LOAD_ASSET_MODEL_NUMBERS
-    }
+    { type: LOAD_ASSET_MODEL_NUMBERS },
+    { type: CREATE_MODEL_NUMBER_SUCCESS },
+    { type: CREATE_MODEL_NUMBER_FAILURE }
   ];
+  let newModel = {
+    make_label: "Sollatex",
+    model_number: "Sollatex GT89"
+  }
+
+  afterEach(() => {
+    store = mockStore({});
+    mock.reset();
+    mock.restore();
+  });
 
   it('should dispatch LOAD_ASSET_MODEL_NUMBERS when loadModelNumbers called successfully', () => {
     mock.onGet(url).reply(200,
@@ -39,6 +56,22 @@ describe('Model Numbers action tests', () => {
     );
     return store.dispatch(loadModelNumbers()).then(() => {
       expect(store.getActions()[0].type).toEqual(expectedActions[0].type);
+    });
+
+  });
+
+  it('should dispatch CREATE_MODEL_NUMBER_SUCCESS when createModelNumbers called successfully', () => {
+    mock.onPost(url, newModel).reply(201,
+      [
+        {
+          "id": 7,
+          "model_number": "Sollatex GT89",
+          "make_label": "Sollatex"
+        }
+      ]
+    );
+    return store.dispatch(createModelNumbers(newModel)).then(() => {
+      expect(store.getActions()[0].type).toEqual(expectedActions[1].type);
     });
   });
 })
