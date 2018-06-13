921f307025ff881defbf06e8d79ab28889ecef47
aimeedyki <akuwudike.a@gmail.com>
2018-06-13 20:02:36 +0100

    fix(sideBar):  fix breaking icons and text on sidebar when resized
    [Fixes #158289377]

--- src/components/DashboardComponent.jsx
+++ src/components/DashboardComponent.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import CardComponent from './CardComponent'
+import CardComponent from './CardComponent';
 import PieChartComponent from './PieChartComponent';
 import BarlineGraphComponent from './BarlineGraphComponent';
 import SideMenuComponent from '../_components/SideMenuComponent';
 import statistics from '../_mock/statistics';
-import { Link } from 'react-router-dom';
 
 import '../_css/DashboardComponent.css';
 
-const DashboardComponent = () => {
-  return (
-    <div>
-    <SideMenuComponent>
+const DashboardComponent = () => (
+  <div>
+    <SideMenuComponent title="Dashboard">
       <CardComponent statistics={statistics} />
       <div className="chartArea">
         <BarlineGraphComponent />
         <PieChartComponent statistics={statistics} />
       </div>
-      </SideMenuComponent>
-      <h1>Dashboard</h1>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><Link to="/asset_types">Asset Types</Link></li>
-      </ul>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><Link to="/feedback">Feedback</Link></li>
-      </ul>
-    </div>
-  )
-};
+    </SideMenuComponent>
+  </div>
+);
 
 export default DashboardComponent;
