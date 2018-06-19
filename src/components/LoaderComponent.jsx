f5507447f3c333f86f58003c072315184ddfe201
rotimi-babalola <rotimi.babalola@andela.com>
2018-06-19 15:34:33 +0100

    ft(loader): - change line break style - fix indenting - fix object-curly-newline eslint rule

--- src/components/LoaderComponent.jsx
+++ src/components/LoaderComponent.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { Dimmer, Loader, Segment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-const LoaderComponent = ({
-  loadingText, size, showDimmer, dimmerStyle
-}) => (
-    <Segment>
-      <Dimmer active={showDimmer} style={dimmerStyle}>
-        <Loader inverted size={size}>{loadingText}</Loader>
-      </Dimmer>
-    </Segment>
-  );
+const LoaderComponent = ({ loadingText, size, showDimmer, dimmerStyle }) => (
+  <Segment>
+    <Dimmer active={showDimmer} style={dimmerStyle}>
+      <Loader inverted size={size}>{loadingText}</Loader>
+    </Dimmer>
+  </Segment>
+);
 
 LoaderComponent.propTypes = {
   loadingText: PropTypes.string,
