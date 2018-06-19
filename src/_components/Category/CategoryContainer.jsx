2cd97ef2074b5c2858ed411dfaf80484e75533e8
EWanjiru <eunice.wanjiru@andela.com>
2018-06-19 16:26:29 +0300

    update category container and component

--- src/_components/Category/CategoryContainer.jsx
+++ src/_components/Category/CategoryContainer.jsx
@@ -1,5 +1,49 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import CategoryComponent from '../../components/Category/AddCategoryComponent';
+import { ToastMessage } from '../../_utils/ToastMessage';
+import { createCategory } from '../../_actions/category.actions';
 
-class ModelNumberContainer extends React.Component {
+class CategoryContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categoryName: ""
+    }
+  }
 
+  handleSubmit = (event, data) => {
+    let newCategory = {
+      "category_name": this.state.categoryName
+    }
+    this.props.createCategory(newCategory)
+    event.target.reset();
+  }
+
+  onAddCategory = (event) => {
+    this.setState({ categoryName: event.target.value });
+  }
+
+  render() {
+    return (
+      <CategoryComponent
+        {...this.props}
+        onaddCategoryName={this.onAddCategory}
+        categoryName={this.state.categoryName}
+        handleSubmit={this.handleSubmit}
+        toggleModal={this.props.toggleModal}
+      />
+    ); 
+  }
 }
+CategoryContainer.propTypes = {
+  createCategory: PropTypes.func.isRequired
+}
+const mapStateToProps = ({ categories }) => ({
+  categories
+});
+export default connect(mapStateToProps, {
+  createCategory
+})(CategoryContainer);
