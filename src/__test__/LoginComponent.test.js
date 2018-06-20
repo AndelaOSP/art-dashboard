f4ba367560a0fd61b69c84204f3e3120103a9a37
andela-fojuri <fisayomi.ojuri@andela.com>
2018-06-20 11:44:38 +0100

    bug(cloudinary): load over https

--- src/__test__/LoginComponent.test.js
+++ src/__test__/LoginComponent.test.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
-import expect from 'expect';
-import { Provider } from 'react-redux';
 import { createMockStore } from 'redux-test-utils';
 import { withRouter } from 'react-router-dom';
 import { shallowWithStore } from 'enzyme-redux';
@@ -16,12 +12,11 @@ window.localStorage = localStorageMock;
 
 
 describe('Renders <LoginComponent /> correctly', () => {
-
   const { WrappedComponent } = withRouter(connect()(LoginComponent));
   const wrapper = shallowWithStore(<WrappedComponent />, createMockStore(null));
 
-  const andelaLogo = 'http://res.cloudinary.com/damc3mj5u/image/upload/v1526571584/logo_uw39tc.png';
-  const googleLogo = 'http://res.cloudinary.com/damc3mj5u/image/upload/v1526571608/google-logo_jjjjqs.svg';
+  const andelaLogo = 'https://res.cloudinary.com/damc3mj5u/image/upload/v1526571584/logo_uw39tc.png';
+  const googleLogo = 'https://res.cloudinary.com/damc3mj5u/image/upload/v1526571608/google-logo_jjjjqs.svg';
 
   it('renders page Container', () => {
     expect(wrapper.find('Container').length).toBe(1);
@@ -52,6 +47,6 @@ describe('Renders <LoginComponent /> correctly', () => {
     );
     wrapper.find('Button').simulate('click');
     wrapper.instance().handleLogin();
-    expect(handleLoginSpy.mock.calls.length).toEqual(1)
+    expect(handleLoginSpy.mock.calls.length).toEqual(1);
   });
 });
