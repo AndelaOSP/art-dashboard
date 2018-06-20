59a010f7fd4cdb44f7759f43d32dd1fec44def14
HawiCaesar <brian.odhiambo@andela.com>
2018-06-20 14:02:59 +0300

    fix(indentation): fix indentation issues

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
 class CardComponent extends React.Component {
-    statistics = this.props.statistics;
-
-    render() {
-        let cards = this.statistics.map((value, index) => {
-            return (
-                <Card key={index}>
-                    <Card.Content>
-                        <Card.Header>{value.value}</Card.Header>
-                        <Card.Description>{value.label}</Card.Description>
-                    </Card.Content>
-                </Card>
-            )
-        });
-
-        return (
-            <Card.Group centered>
-                {cards}
-            </Card.Group>
-        )
-    }
+  statistics = this.props.statistics;
+  render() {
+    const cards = this.statistics.map(value => (
+      <Card key={value.value}>
+        <Card.Content>
+          <Card.Header>{value.value}</Card.Header>
+          <Card.Description>{value.label}</Card.Description>
+        </Card.Content>
+      </Card>
+    ));
+    return (
+      <Card.Group centered>
+        {cards}
+      </Card.Group>
+    );
+  }
 }
+
+CardComponent.propTypes = {
+  statistics: PropTypes.array
+};
+
+CardComponent.defauktTypes = {
+  statistics: []
+};
 
 export default CardComponent;
