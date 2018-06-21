1eee762e1e78ecd97420418aa69161aed5f19bfe
EWanjiru <eunice.wanjiru@andela.com>
2018-06-21 12:11:00 +0300

    add a create category action test

--- src/__test__/actions/category.actions.test.js
+++ src/__test__/actions/category.actions.test.js
@@ -7,10 +7,11 @@ import thunk from 'redux-thunk';
 
 // constants
 import constants from '../../_constants';
-const { LOAD_CATEGORIES_SUCCESS } = constants;
 
 // actions
-import { loadCategories } from '../../_actions/category.actions';
+import { loadCategories, createCategory } from '../../_actions/category.actions';
+
+const { LOAD_CATEGORIES_SUCCESS } = constants;
 
 // store
 const middleware = [thunk];
@@ -19,9 +20,9 @@ let store;
 
 describe('Category action tests', () => {
   const mock = new MockAdapter(axios);
-  let url = 'https://my-json-server.typicode.com/HawiCaesar/jsonplaceholders-demo/categories';
+  const url = 'categories';
   store = mockStore({});
-  let expectedActions = [
+  const expectedActions = [
     {
       type: LOAD_CATEGORIES_SUCCESS
     }
@@ -31,12 +32,12 @@ describe('Category action tests', () => {
     mock.onGet(url).reply(200,
       [
         {
-          "id": 1,
-          "category_name": "Accessories"
+          id: 1,
+          category_name: 'Accessories'
         },
         {
-          "id": 2,
-          "category_name": "Electronics"
+          id: 2,
+          category_name: 'Electronics'
         }
       ]
     );
@@ -44,4 +45,30 @@ describe('Category action tests', () => {
       expect(store.getActions()[0].type).toEqual(expectedActions[0].type);
     });
   });
+
+
+  it('should dispatch CREATE_CATEGORY_SUCCESS when createCategory is called successfully', () => {
+    store = mockStore({ categories: [] });
+    const newCategory = {
+      category_name: 'Tesy Category',
+    };
+
+    const expectedAction = [{
+      type: 'CREATE_CATEGORY_SUCCESS',
+      payload: { id: 4, category_name: 'Tesy Category' }
+    }];
+
+    mock
+      .onPost(url, newCategory)
+      .reply(201,
+        {
+          id: 4,
+          category_name: 'Tesy Category'
+        }
+      );
+    return store.dispatch(createCategory(newCategory)).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
 });
+
