895b2ff76cf6ec5ee23ab0274af62ffa7aaad3c0
EWanjiru <eunice.wanjiru@andela.com>
2018-06-21 15:27:56 +0300

    add create category test

--- src/__test__/reducers/category.reducer.test.js
+++ src/__test__/reducers/category.reducer.test.js
@@ -1,6 +1,5 @@
 // third-party library
 import expect from 'expect';
-
 // reducer
 import categoryReducer from '../../_reducers/category.reducer';
 
@@ -10,12 +9,21 @@ import { mockStore } from '../../_mock/mockStore';
 // mock data
 import categories from '../../_mock/categories';
 
-import { loadCategoriesSuccess } from '../../_actions/category.actions';
+import { loadCategoriesSuccess, createCategorySuccess } from '../../_actions/category.actions';
+
 
 describe('Category Reducer tests', () => {
-  it('should handle CREATE_ASSET_SUCCESS', () => {
-    let action = loadCategoriesSuccess(categories);
+  it('should handle LOAD_CATEGORIES_SUCCESS', () => {
+    const action = loadCategoriesSuccess(categories);
     expect(mockStore.categories.length).toEqual(0);
     expect(categoryReducer(mockStore.categories, action)).toEqual(categories);
+  });
+
+  it('should handle CREATE_CATEGORY_SUCCESS', () => {
+    const newCategory = { id: 4, category_name: 'Tesy Category' };
+    const expected = [newCategory];
+    const action = createCategorySuccess(newCategory);
+    expect(mockStore.categories.length).toEqual(0);
+    expect(categoryReducer(mockStore.categories, action)).toEqual(expected);
   });
 });
