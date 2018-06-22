632ee51b5e36666960584c1e44d905699a898641
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-22 16:58:48 +0300

    add reducer

--- src/_reducers/category.reducer.js
+++ src/_reducers/category.reducer.js
@@ -1,12 +1,14 @@
 import constants from '../_constants';
 import initialState from './initialState';
 
-const { LOAD_CATEGORIES_SUCCESS } = constants;
+const { LOAD_CATEGORIES_SUCCESS, CREATE_CATEGORY_SUCCESS } = constants;
 
 export default (state = initialState.categories, action) => {
   switch (action.type) {
     case LOAD_CATEGORIES_SUCCESS:
       return action.payload;
+    case CREATE_CATEGORY_SUCCESS:
+      return [...state, action.payload];
     default:
       return state;
   }
