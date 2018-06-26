1f6ee22f36596f07b2ebf76f00dabffced3edd5f
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-26 14:52:02 +0300

    The outer div is not needed. <Form> is already a parent component

--- src/components/Category/AddCategoryComponent.jsx
+++ src/components/Category/AddCategoryComponent.jsx
@@ -7,20 +7,18 @@ import InputFluid from '../common/TextInputComponent';
 import '../../_css/AddAssetComponent.css';
 
 const AddCategoryComponent = props => (
-  <div>
-    <Form onSubmit={props.handleSubmit}>
-      <Label htmlFor="category" className="label-style">Category Name</Label>
-      <InputFluid
-        name="category-name"
-        id="category"
-        onChange={props.onaddCategoryName}
-        placeholder="Enter Category Name"
-      />
-      <br />
-      <ArtButton buttonName="Save" color="primary" />
-      <ArtButton buttonName="Cancel" onClick={props.toggleModal} />
-    </Form>
-  </div>
+  <Form onSubmit={props.handleSubmit}>
+    <Label htmlFor="category" className="label-style">Category Name</Label>
+    <InputFluid
+      name="category-name"
+      id="category"
+      onChange={props.onaddCategoryName}
+      placeholder="Enter Category Name"
+    />
+    <br />
+    <ArtButton buttonName="Save" color="primary" />
+    <ArtButton buttonName="Cancel" onClick={props.toggleModal} />
+  </Form>
 );
 
 AddCategoryComponent.propTypes = {
