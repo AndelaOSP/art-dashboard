b7349667a9b9feb94600783614723e400d51e47a
Joan Awinja Ingari <joan.awinja@andela.com>
2018-06-27 12:42:00 +0300

    fixed spaces

--- src/_components/Category/CategoryContainer.jsx
+++ src/_components/Category/CategoryContainer.jsx
@@ -7,31 +7,31 @@ import { ToastMessage } from '../../_utils/ToastMessage';
 import resetToastMessageContent from '../../_actions/resetToastMessage.actions';
 
 class CategoryContainer extends React.Component {
-      state = {
+  state = {
+    categoryName: '',
+    saveButtonState: false
+  };
+
+  static getDerivedStateFromProps(nextProps) {
+    if (nextProps.toastMessageContent.type) {
+      if (nextProps.toastMessageContent.type === 'success') {
+        ToastMessage.success({
+          message: nextProps.toastMessageContent.message
+        });
+      } else if (nextProps.toastMessageContent.type === 'error') {
+        ToastMessage.error({
+          message: nextProps.toastMessageContent.message
+        });
+      }
+      nextProps.resetToastMessageContent();
+      nextProps.toggleModal();
+      return {
         categoryName: '',
         saveButtonState: false
       };
-
-      static getDerivedStateFromProps(nextProps) {
-        if (nextProps.toastMessageContent.type) {
-          if (nextProps.toastMessageContent.type === 'success') {
-            ToastMessage.success({
-              message: nextProps.toastMessageContent.message
-            });
-          } else if (nextProps.toastMessageContent.type === 'error') {
-            ToastMessage.error({
-              message: nextProps.toastMessageContent.message
-            });
-          }
-          nextProps.resetToastMessageContent();
-          nextProps.toggleModal();
-          return {
-            categoryName: '',
-            saveButtonState: false
-          };
-        }
-        return null;
-      }
+    }
+    return null;
+  }
   onChangeButtonState = () => {
     this.setState({ saveButtonState: !this.state.saveButtonState });
   };
