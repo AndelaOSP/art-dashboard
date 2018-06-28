1d2439434a5b5757f55609b5d93d3190b6cb7e26
aimeedyki <akuwudike.a@gmail.com>
2018-06-28 22:55:35 +0100

    chore(authentication): refresh expired token on request

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import 'semantic-ui-css/semantic.min.css';
-import { refreshUserToken } from '../src/firebase';
+import setAuthorizationConfig from '../src/_utils/setAuthorizationConfig';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-refreshUserToken();
-const AUTH_TOKEN = localStorage.getItem('art-prod-web-token');
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.headers.common.Authorization = `Token ${AUTH_TOKEN}`;
+
+if (localStorage.getItem('art-prod-web-token')) {
+  setAuthorizationConfig();
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
