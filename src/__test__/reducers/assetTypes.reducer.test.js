7e5b88fa45df22b0c0ced09487754f32413b91dd
Faith <faith.mustapha@andela.com>
2018-06-29 14:13:17 +0100

    chore(assetTypes): rename test file for asset types reducer to right name                    and add test cases
    [#158491088]

---
+++ src/__test__/reducers/assetTypes.reducer.test.js
@@ -0,0 +1,39 @@
+// third-party library
+import expect from 'expect';
+import constants from '../../_constants';
+import assetTypesReducer from '../../_reducers/assetTypes.reducer';
+import assetTypesMock from '../../_mock/assetTypes';
+
+const { LOAD_ASSET_TYPES_SUCCESS, LOAD_ASSET_TYPES_FAILURE, LOADING_ASSET_TYPES } = constants;
+
+const initialState = {
+  assetTypes: [],
+  isLoading: false
+};
+
+const action = {};
+
+describe('Asset Type Reducer tests', () => {
+  it('should return initial state when there is no action', () => {
+    expect(assetTypesReducer(initialState, action)).toEqual(initialState);
+  });
+
+  it('should handle LOAD_ASSET_TYPES_SUCCESS', () => {
+    action.type = LOAD_ASSET_TYPES_SUCCESS;
+    action.payload = assetTypesMock;
+    expect(assetTypesReducer(initialState, action).assetTypes).toEqual(action.payload);
+    expect(assetTypesReducer(initialState, action).isLoading).toEqual(false);
+  });
+
+  it('should handle LOAD_ASSET_TYPES_FAILURE', () => {
+    action.type = LOAD_ASSET_TYPES_FAILURE;
+    expect(assetTypesReducer(initialState, action).assetTypes).toEqual([]);
+    expect(assetTypesReducer(initialState, action).isLoading).toEqual(false);
+  });
+
+  it('should handle LOADING_ASSET_TYPES', () => {
+    action.type = LOADING_ASSET_TYPES;
+    expect(assetTypesReducer(initialState, action).assetTypes).toEqual([]);
+    expect(assetTypesReducer(initialState, action).isLoading).toEqual(true);
+  });
+});
