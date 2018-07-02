a0ae1225a29789c4bf0957390ba9612588e5a47a
HawiCaesar <brian.odhiambo@andela.com>
2018-07-02 16:21:00 +0300

    feat(assetTypes): add components, actions & reducer

--- src/_actions/assetTypes.actions.js
+++ src/_actions/assetTypes.actions.js
@@ -7,7 +7,8 @@ const {
   LOAD_ASSET_TYPES_FAILURE,
   LOADING_ASSET_TYPES,
   CREATE_ASSET_TYPE_SUCCESS,
-  CREATE_ASSET_TYPE_FAILURE
+  CREATE_ASSET_TYPE_FAILURE,
+  UPDATE_TOAST_MESSAGE_CONTENT
 } = constants;
 
 export const loadAssetTypes = pageNumber => (dispatch) => {
@@ -23,15 +24,17 @@ export const loadAssetTypes = pageNumber => (dispatch) => {
 };
 
 export const createAssetType = newAssetType =>
-  dispatch => axios.post('asset-types', newAssetType).then((response) => {
-    dispatch(createAssetTypeSuccess(response.data));
-    dispatch(updateToastMessageContent('Asset Type Saved Successfully',
-      'success'));
-  }).catch((error) => {
-    dispatch(createAssetTypeFailure(error));
-    dispatch(updateToastMessageContent('Could Not Save The Asset Type',
-      'error'));
-  });
+  (dispatch) => {
+    axios.post('asset-types/', newAssetType).then((response) => {
+      dispatch(createAssetTypeSuccess(response.data));
+      dispatch(updateToastMessageContent('Asset Type Saved Successfully',
+        'success'));
+    }).catch((error) => {
+      dispatch(createAssetTypeFailure(error));
+      dispatch(updateToastMessageContent('Could Not Save The Asset Type',
+        'error'));
+    });
+  };
 
 export const createAssetTypeSuccess = assetType => (
   { type: CREATE_ASSET_TYPE_SUCCESS, payload: assetType }
