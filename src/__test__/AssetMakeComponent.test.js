640b4e476b2b4dbd3babaa57b4661cf1433b051a
EWanjiru <eunice.wanjiru@andela.com>
2018-07-05 19:19:25 +0300

    update component tests

--- src/__test__/AssetMakeComponent.test.js
+++ src/__test__/AssetMakeComponent.test.js
@@ -3,20 +3,26 @@ import { mount } from 'enzyme';
 import expect from 'expect';
 import { spy } from 'sinon';
 import { AddAssetMakeComponent } from '../components/AssetMake/AddAssetMakeComponent';
+import AssetMakeContainer from '../_components/AssetMake/AssetMakeContainer';
+
 
 const toggleModal = spy();
 const onChangeButtonState = spy();
+const props = {
+  toggleModal,
+  handleSubmit: jest.fn(),
+  onChangeButtonState,
+  onaddAssetMake: jest.fn(),
+  assetTypes: [],
+  onSelectAssetType: jest.fn(),
+  addAssetMakes: jest.fn(),
+  loadAssetTypes: jest.fn(),
+  loadAssetMakes: jest.fn(),
+  resetToastMessageContent: jest.fn(),
+  toastMessageContent: {}
+};
 
 describe('Renders <AddAssetMakeComponent /> correctly', () => {
-  const props = {
-    toggleModal,
-    handleSubmit: jest.fn(),
-    onChangeButtonState,
-    onaddAssetMake: jest.fn(),
-    assetTypes: [],
-    onSelectAssetType: jest.fn()
-  };
-
   const wrapper = mount(<AddAssetMakeComponent {...props} />);
 
   it('Should find the form', () => {
@@ -45,5 +51,46 @@ describe('Renders <AddAssetMakeComponent /> correctly', () => {
     expect(wrapper.find('.cancel-button').length).toEqual(1);
     wrapper.find('.cancel-button').simulate('click');
     expect(toggleModal.callCount).toEqual(1);
+  });
+});
+
+describe('Renders <AssetMakeContainer /> correctly', () => {
+  const wrapper = mount(<AssetMakeContainer.WrappedComponent {...props} />);
+  const instance = wrapper.instance();
+  instance.setState({
+    assetMake: '',
+    assetType: '',
+    saveButtonState: false
+  });
+
+  it('should render <AddAssetMakeComponent />', () => {
+    expect(wrapper.find(AddAssetMakeComponent).length).toEqual(1);
+  });
+
+  it('should call onChangeButtonState when Save Button is clicked', () => {
+    instance.onChangeButtonState();
+    expect(instance.state.saveButtonState).toEqual(true);
+  });
+
+  it('should call onAddAssetMake when asset Make input field content changes', () => {
+    const event = { target: { value: 'Microsoft' } };
+    instance.onAddAssetMake(event);
+    expect(instance.state.assetMake).toEqual('Microsoft');
+  });
+
+  it('should call onSelectAssetType when DropdownComponent changes', () => {
+    const event = { target: '', data: { name: 'Laptops', value: 1 } };
+    instance.onSelectAssetType(event, event.data);
+    expect(instance.state.assetType).toEqual(1);
+  });
+
+  it('should receive a message type prop when getDerivedStateFromProps loads', () => {
+    wrapper.setProps({
+      toastMessageContent: {
+        type: 'success',
+        message: 'A success message'
+      }
+    });
+    expect(wrapper.props().toastMessageContent.type).toEqual('success');
   });
 });
