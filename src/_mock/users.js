105403b4d7f645dcfb948b75bb2a31ca0b1b555c
EWanjiru <eunice.wanjiru@andela.com>
2018-07-13 04:05:33 +0300

    add reducer and action tests

---
+++ src/_mock/users.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    first_name: 'Test',
+    last_name: 'User',
+    email: 'test@email.com',
+    cohort: 10
+  },
+  {
+    id: 2,
+    first_name: 'Test2',
+    last_name: 'User2',
+    email: 'test2@email.com',
+    cohort: 9
+  }
+];
+
