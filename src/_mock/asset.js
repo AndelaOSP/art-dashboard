f545de97bff87bac5d9e20c2c0e9f6d8c2051497
Faith <faith.mustapha@andela.com>
2018-07-27 13:19:40 +0100

    feat(Unassign-user): add reducer tests
    [Finishes: #159022597]

--- src/_mock/asset.js
+++ src/_mock/asset.js
@@ -1,25 +1,51 @@
-export default {
-  "id": 1,
-  "asset_code": "AND/HS/0909",
-  "serial_number": "897832SWWS",
-  "model_number": 6,
-  "checkin_status": "string",
-  "assigned_to": {
-    "id": 4,
-    "first_name": "John",
-    "last_name": "Kamau",
-    "full_name": "string",
-    "email": "joekamau@andela.com",
-    "cohort": 0,
-    "slack_handle": "jkamau",
-    "picture": "string",
-    "phone_number": "0701000100",
-    "last_modified": "2018-05-28T10:28:13Z",
-    "date_joined": "2018-05-28T10:28:13Z",
-    "last_login": "2018-05-28T10:28:13Z"
+export const asset = {
+  id: 1,
+  asset_code: 'AND/HS/0909',
+  serial_number: '897832SWWS',
+  model_number: 6,
+  checkin_status: 'string',
+  assigned_to: {
+    id: 4,
+    first_name: 'John',
+    last_name: 'Kamau',
+    full_name: 'string',
+    email: 'joekamau@andela.com',
+    cohort: 0,
+    slack_handle: 'jkamau',
+    picture: 'string',
+    phone_number: '0701000100',
+    last_modified: '2018-05-28T10:28:13Z',
+    date_joined: '2018-05-28T10:28:13Z',
+    last_login: '2018-05-28T10:28:13Z'
   },
-  "created_at": "2018-05-28T10:28:13Z",
-  "last_modified": "2018-05-28T10:28:13Z",
-  "current_status": "Available",
-  "asset_type": "Headsets"
-}
+  created_at: '2018-05-28T10:28:13Z',
+  last_modified: '2018-05-28T10:28:13Z',
+  current_status: 'Available',
+  asset_type: 'Headsets'
+};
+
+export const newAllocation = {
+  current_owner: 'humphrey.thuo@andela.com',
+  created_at: '2018-07-27T09:31:03.930553Z'
+};
+
+export const unAssignedAsset = {
+  id: 631,
+  status_history: [
+    {
+      id: 630,
+      asset: 27,
+      current_status: 'Available',
+      previous_status: 'Available',
+      created_at: '2018-07-23T15:00:17.330827Z'
+    },
+    {
+      id: 627,
+      asset: 27,
+      current_status: 'Available',
+      previous_status: 'Available',
+      created_at: '2018-07-27T09:25:37.185019Z'
+    }
+  ],
+  created_at: '2018-07-27T09:36:40.710348Z'
+};
