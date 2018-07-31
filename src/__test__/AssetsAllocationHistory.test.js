b41a7cc201d571fe19f0a8a922f78c60900e9142
EWanjiru <eunice.wanjiru@andela.com>
2018-08-01 00:26:27 +0300

    feat(allocation-history): increase test coverage  - add test for data formatting
    [finishes #159022593]

--- src/__test__/AssetsAllocationHistory.test.js
+++ src/__test__/AssetsAllocationHistory.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import expect from 'expect';
-
 import AssetAllocationHistory from '../components/AssetAllocationHistory';
 
 describe('Renders <AssetAllocationHistory /> correctly', () => {
@@ -23,5 +22,19 @@ describe('Renders <AssetAllocationHistory /> correctly', () => {
       }]
     });
     expect(wrapper.find('Table').length).toBe(1);
+    expect(wrapper.find('TableRowComponent').length).toBe(1);
+  });
+
+  it('sets the allocation history previous_owner or current_owner to - if null and formats date', () => {
+    wrapper.setProps({
+      allocationHistory: [{
+        created_at: '2018-07-31T13:11:24.054616Z',
+        current_owner: null,
+        previous_owner: null
+      }]
+    });
+    expect(wrapper.find('TableRowComponent').props().data.created_at).toBe('Tuesday, July 31, 2018');
+    expect(wrapper.find('TableRowComponent').props().data.current_owner).toBe(' - ');
+    expect(wrapper.find('TableRowComponent').props().data.previous_owner).toBe(' - ');
   });
 });
