6edf60aa6889de56399c8221b06641e6f631ba81
EWanjiru <eunice.wanjiru@andela.com>
2018-07-31 22:42:39 +0300

    feat(allocation-history): correct some styling  - add whitespace before opening brace
    [#159022593]

--- src/components/AssetAllocationHistory.jsx
+++ src/components/AssetAllocationHistory.jsx
@@ -16,39 +16,38 @@ const AssetAllocationHistory = ({ allocationHistory }) => {
   }
 
   return (
-    <div className="allocations-history">
-      <Container>
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Date</Table.HeaderCell>
-              <Table.HeaderCell>Current Owner</Table.HeaderCell>
-              <Table.HeaderCell>Previous Owner</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+    <Container>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+            <Table.HeaderCell>Current Owner</Table.HeaderCell>
+            <Table.HeaderCell>Previous Owner</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-          <Table.Body>
-            {
-              allocationHistory.map((allocationRecord) => {
-                if (allocationRecord.previous_owner === null) {
-                  allocationRecord.previous_owner = ' - ';
-                }
-                if (allocationRecord.current_owner === null) {
-                  allocationRecord.current_owner = ' - ';
-                }
-                allocationRecord.created_at = FormatDate(allocationRecord.created_at);
-                return (
-                  <TableRowComponent
-                    key={allocationRecord.id}
-                    data={allocationRecord}
-                    headings={['created_at', 'current_owner', 'previous_owner']}
-                  />);
-              })
-            }
-          </Table.Body>
-        </Table>
-      </Container>
-    </div>
+        <Table.Body>
+          {
+            allocationHistory.map((allocationRecord) => {
+              if (allocationRecord.previous_owner === null) {
+                allocationRecord.previous_owner = ' - ';
+              }
+              if (allocationRecord.current_owner === null) {
+                allocationRecord.current_owner = ' - ';
+              }
+              allocationRecord.created_at = FormatDate(allocationRecord.created_at);
+              return (
+                <TableRowComponent
+                  key={allocationRecord.id}
+                  data={allocationRecord}
+                  headings={['created_at', 'current_owner', 'previous_owner']}
+                />
+              );
+            })
+          }
+        </Table.Body>
+      </Table>
+    </Container>
   );
 };
 
