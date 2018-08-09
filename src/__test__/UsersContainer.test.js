013fe77cd1e8ae2295562603ab0a0cc81107a58a
Zacharia <zacmwangi94@gmail.com>
2018-08-09 13:17:39 +0300

    chore(refactor): Refactor test

---
+++ src/__test__/UsersContainer.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expect from 'expect';
+import UserContainer from '../components/UserDetailsContainer';
+
+const props = {
+  loadUsers: jest.fn(),
+  hasError: false,
+  isLoading: false,
+  usersCount: 10,
+  errorMessage: '',
+  users: []
+};
+
+describe('Renders <UserDetailsContainer /> correctly', () => {
+  const wrapper = shallow(<UserContainer.WrappedComponent {...props} />);
+
+  it('renders the UserDetailsComponent component', () => {
+    expect(wrapper.find('UserDetailsComponent').length).toBe(1);
+  });
+
+  it('renders page title', () => {
+    expect(wrapper.find('.page-headings').prop('content')).toEqual('Users List');
+  });
+});
