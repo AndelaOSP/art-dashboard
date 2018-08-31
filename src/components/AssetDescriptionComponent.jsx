cdb250c4441d97707b79a7796347fcf8fc1c3ee7
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-08-31 17:24:07 +0300

    fix(customize-modal): make changes to customize modal
    [#160064437]

--- src/components/AssetDescriptionComponent.jsx
+++ src/components/AssetDescriptionComponent.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { isEmpty, values } from 'lodash';
 import { Container, Header, Grid } from 'semantic-ui-react';
-import AssignAssetModal from './AssignAssetModal';
+import ModalComponent from './common/ModalComponent';
+import ButtonComponent from '../components/common/ButtonComponent';
 import DropdownComponent from '../components/common/DropdownComponent';
 import '../_css/AssetDescriptionComponent.css';
 
@@ -24,36 +25,67 @@ const AssetDescriptionComponent = props => (
         </div>
       </Grid.Column>
       <Grid.Column>
-        {
-          (!isEmpty(values(props.assignedUser))) ?
-            <div id="allocate-asset">
-              <Header as="h3" content="Assigned To:" />
-              <div
-                id="email"
-                className="asset-specs"
-              >
-                {props.assignedUser.email}
+        <ModalComponent
+          trigger={
+            (!isEmpty(values(props.assignedUser))) ?
+              <div id="allocate-asset">
+                <Header as="h3" content="Assigned To:" />
+                <div
+                  id="email"
+                  className="asset-specs"
+                >
+                  {props.assignedUser.email}
+                </div>
+                <br />
+                <ButtonComponent
+                  buttonName="Unassign Asset"
+                  customCss="unassign-asset"
+                  color="primary"
+                />
               </div>
-              <br />
-              <AssignAssetModal {...props} />
-            </div>
-            :
-            <div id="allocate-asset">
-              <Header as="h3" content="Assign this asset to:" />
-              <DropdownComponent
-                customClass="form-dropdown"
-                label="Assign this asset to:"
-                placeHolder="Select Andela Email To Assign This Asset"
-                name="assign-user"
-                search
-                value={props.selectedUserId}
-                onChange={props.onSelectUserEmail}
-                options={userEmailsOptions(props.users)}
-              />
-              <br />
-              <AssignAssetModal {...props} />
-            </div>
-        }
+              :
+              <div id="allocate-asset">
+                <Header as="h3" content="Assign this asset to:" />
+                <DropdownComponent
+                  customClass="form-dropdown"
+                  label="Assign this asset to:"
+                  placeHolder="Select Andela Email To Assign This Asset"
+                  name="assign-user"
+                  search
+                  value={props.selectedUserId}
+                  onChange={props.onSelectUserEmail}
+                  options={userEmailsOptions(props.users)}
+                />
+                <br />
+                <ButtonComponent
+                  buttonName="Assign Asset"
+                  customCss="assign-asset"
+                  disabledState={props.assignAssetButtonState}
+                  color="primary"
+                />
+              </div>
+          }
+          modalTitle="Confirm Action"
+        >
+          <div>
+            <label>
+              Are you sure you want to perform this action?
+            </label>
+            <br />
+            <ButtonComponent
+              className="cancel"
+              buttonName="Cancel"
+              handleClick={props.toggleModal}
+            />
+            <ButtonComponent
+              className="save"
+              buttonName="Save"
+              color="primary"
+              buttonState={props.buttonState}
+              handleClick={props.handleConfirm}
+            />
+          </div>
+        </ModalComponent>
       </Grid.Column>
     </Grid>
   </Container>
@@ -63,7 +95,11 @@ AssetDescriptionComponent.propTypes = {
   onSelectUserEmail: PropTypes.func,
   assignedUser: PropTypes.object,
   users: PropTypes.array,
-  selectedUserId: PropTypes.number
+  selectedUserId: PropTypes.number,
+  toggleModal: PropTypes.func.isRequired,
+  handleConfirm: PropTypes.func.isRequired,
+  buttonState: PropTypes.bool.isRequired,
+  assignAssetButtonState: PropTypes.bool.isRequired
 };
 
 AssetDescriptionComponent.defaultProps = {
