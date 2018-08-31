75c04b26621517efcede43c2189dc2f63302cffe
andela-fojuri <fisayomi.ojuri@andela.com>
2018-08-31 08:50:18 -0500

    [Feature #157340077] Code Refactor

--- src/_utils/formValidation.js
+++ src/_utils/formValidation.js
@@ -1,6 +1,6 @@
 const emailValidation = (value) => {
   const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]? w+)*(\.\w{2,3})+$/;
-  return !!emailRegex.test(value);
+  return emailRegex.test(value);
 };
 
 export default emailValidation;
