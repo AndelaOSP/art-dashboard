a5e31848cc4a9b942beb798339d84596c1125f60
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-09-05 01:09:22 +0300

    fix(tests): fix failing coverage
    [#160064437]

--- src/__test__/ConfirmAction.test.js
+++ src/__test__/ConfirmAction.test.js
@@ -6,15 +6,23 @@ describe('Renders <ConfirmAction /> correctly', () => {
   const props = {
     toggleModal: jest.fn(),
     handleConfirm: jest.fn(),
-    buttonState: false
+    buttonState: false,
+    buttonLoading: true
   };
   const wrapper = mount(<ConfirmAction {...props} />);
 
-  it('componentDidUpdate is called on change in props', () => {
+  it('should call componentDidUpdate', () => {
     const componentDidUpdateSpy = jest.spyOn(
       wrapper.instance(), 'componentDidUpdate'
     );
     wrapper.setProps({ buttonState: true });
     expect(componentDidUpdateSpy.mock.calls.length).toEqual(1);
+  });
+
+  it('should call toggleModal when buttonLoading is false ', () => {
+    const prevProps = props;
+    wrapper.instance().componentDidUpdate(prevProps);
+    wrapper.setProps({ buttonLoading: false });
+    expect(props.toggleModal.mock.calls.length).toEqual(1);
   });
 });
