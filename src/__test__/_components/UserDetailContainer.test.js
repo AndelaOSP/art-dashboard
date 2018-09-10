df908e6a184374b9580f2e832d5949fe164e6de4
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-09-10 18:03:29 +0300

    feat(view-user): add tests
    [finishes #160136598]

---
+++ src/__test__/_components/UserDetailContainer.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expect from 'expect';
+import UserDetailContainer from '../../_components/User/UserDetailContainer';
+
+const props = {
+  loadUserDetail: jest.fn(),
+  isLoading: false,
+  hasError: false,
+  errorMessage: '',
+  userDetail: {},
+  match: {
+    params: { id: 108 }
+  }
+};
+
+describe('Renders <UserDetailContainer /> correctly', () => {
+  const wrapper = shallow(<UserDetailContainer.WrappedComponent {...props} />);
+
+  it('calls componentDidMount', () => {
+    wrapper.setProps({
+      userDetail: {
+        full_name: 'test name',
+        email: 'test@email.com',
+        cohort: 16,
+        slack_handle: 'test',
+        phone_number: 1234567
+      }
+    });
+    const componentDidMountSpy = jest.spyOn(
+      wrapper.instance(), 'componentDidMount'
+    );
+    wrapper.instance().componentDidMount();
+    expect(componentDidMountSpy.mock.calls.length).toEqual(1);
+  });
+
+  it('renders the UserDetailComponent ', () => {
+    wrapper.setProps({
+      userDetail: {
+        full_name: 'test name',
+        email: 'test@email.com',
+        cohort: 16,
+        slack_handle: 'test',
+        phone_number: 1234567
+      }
+    });
+
+    expect(wrapper.find('UserDetailComponent').exists()).toEqual(true);
+  });
+});
