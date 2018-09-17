1a81cc1ac20cef0eaa182380e485124649445e1e
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-09-17 12:26:20 +0300

    fix(test): fix failing test
    [finishes #160477991]

--- src/__test__/components/UserDetailComponent.test.js
+++ src/__test__/components/UserDetailComponent.test.js
@@ -23,10 +23,39 @@ describe('Renders <UserDetailComponent /> correctly', () => {
         email: 'test@email.com',
         cohort: 16,
         slack_handle: 'test',
-        phone_number: 1234567
+        phone_number: 1234567,
+        allocated_assets: [
+          {
+            id: 55,
+            asset_type: 'Computer',
+            asset_code: 'AND/RE/34',
+            serial_number: 'GT2342342'
+          },
+          {
+            id: 56,
+            asset_type: 'Dongle',
+            asset_code: 'AND/DNG/43',
+            serial_number: 'DNG90909903'
+          }
+        ]
       },
       isLoading: false
     });
     expect(wrapper.find('Table').exists()).toEqual(true);
+  });
+
+  it('renders an empty card if allocated_assets is empty ', () => {
+    wrapper.setProps({
+      userDetail: {
+        full_name: 'test name',
+        email: 'test@email.com',
+        cohort: 16,
+        slack_handle: 'test',
+        phone_number: 1234567,
+        allocated_assets: []
+      },
+      isLoading: false
+    });
+    expect(wrapper.find('Card').exists()).toEqual(true);
   });
 });
