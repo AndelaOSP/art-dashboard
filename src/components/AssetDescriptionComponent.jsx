0ad29507a901071fbce6f7600fb5b271ae04fc85
Thuku <thuku.bonnie@gmail.com>
2018-09-26 10:13:12 +0300

    bug(asset detail): refactor asset detail)   - fix indentation on asset detail component

--- src/components/AssetDescriptionComponent.jsx
+++ src/components/AssetDescriptionComponent.jsx
@@ -19,91 +19,92 @@ class AssetDescriptionComponent extends React.Component {
    selectedUser: 0
  }
 
- onSelectUserEmail = (event, data) => {
-   this.setState({ selectedUser: data.value, assignAssetButtonState: false });
- };
-
-handleAssign = () => {
-  const { selectedUser } = this.state;
-  const { id } = this.props.assetDetail;
-  const assetAllocated = {
-    asset: id,
-    current_owner: selectedUser
+  onSelectUserEmail = (event, data) => {
+    this.setState({ selectedUser: data.value, assignAssetButtonState: false });
   };
-  this.props.allocateAsset(assetAllocated, this.props.serialNumber);
-};
 
-handleUnassign = () => {
-  const { id } = this.props.assetDetail;
-  const assetAssigned = {
-    asset: id,
-    current_status: 'Available'
+  handleAssign = () => {
+    const { selectedUser } = this.state;
+    const { id } = this.props.assetDetail;
+    const assetAllocated = {
+      asset: id,
+      current_owner: selectedUser
+    };
+    this.props.allocateAsset(assetAllocated, this.state.serialNumber);
   };
-  this.props.unassignAsset(assetAssigned, this.props.serialNumber);
-};
 
-handleConfirm = () => {
-  if (isEmpty(values(this.state.assignedUser))) {
-    return this.handleAssign();
-  }
-  return this.handleUnassign();
-};
+  handleUnassign = () => {
+    const { id } = this.props.assetDetail;
+    const assetAssigned = {
+      asset: id,
+      current_status: 'Available'
+    };
+    this.props.unassignAsset(assetAssigned, this.props.serialNumber);
+  };
 
-triggerProps = () => {
-  if (isEmpty(values(this.state.assignedUser))) {
+  handleConfirm = () => {
+    if (isEmpty(values(this.state.assignedUser))) {
+      return this.handleAssign();
+    }
+    return this.handleUnassign();
+  };
+
+  triggerProps = () => {
+    if (isEmpty(values(this.state.assignedUser))) {
+      return {
+        buttonName: 'Assign Asset',
+        customCss: 'assign-asset',
+        disabledState: this.state.assignAssetButtonState,
+        color: 'primary'
+      };
+    }
+
     return {
-      buttonName: 'Assign Asset',
-      customCss: 'assign-asset',
-      disabledState: this.state.assignAssetButtonState,
+      buttonName: 'Unassign Asset',
+      customCss: 'unassign-asset',
+      disabledState: false,
       color: 'primary'
     };
   }
-  return {
-    buttonName: 'Unassign Asset',
-    customCss: 'unassign-asset',
-    disabledState: false,
-    color: 'primary'
-  };
-}
 
-render() {
-  const { onSelectUserEmail, assignedUser } = this.state;
-  const { users, selectedUserId, toggleModal, buttonState, buttonLoading } = this.props;
-  const triggerProps = this.triggerProps();
-  return (
-    <Container>
-      <Grid columns={2} stackable className="asset-description">
-        <Grid.Column>
-          <Header as="h3" content="Asset Specs" />
-          <div className="asset-specs">
-            12-inch (diagonal) LED-backlit display with IPS technology
-            2304-by-1440 resolution at 226 pixels per inch with support for millions of colors
-            16:10 aspect ratio
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <AssignedTo
-            onSelectUserEmail={onSelectUserEmail}
-            assignedUser={assignedUser}
-            users={users}
-            selectedUserId={selectedUserId}
-          />
-          <ModalComponent
-            trigger={<ButtonComponent {...triggerProps} />}
-            modalTitle="Confirm Action"
-          >
-            <ConfirmAction
-              toggleModal={toggleModal}
-              handleConfirm={this.handleConfirm}
-              buttonState={buttonState}
-              buttonLoading={buttonLoading}
+  render() {
+    const { onSelectUserEmail, assignedUser } = this.state;
+    const { users, selectedUserId, toggleModal, buttonState, buttonLoading } = this.props;
+    const triggerProps = this.triggerProps();
+    return (
+      <Container>
+        <Grid columns={2} stackable className="asset-description">
+          <Grid.Column>
+            <Header as="h3" content="Asset Specs" />
+            <div className="asset-specs">
+              12-inch (diagonal) LED-backlit display with IPS technology
+              2304-by-1440 resolution at 226 pixels per inch with support for millions of colors
+              16:10 aspect ratio
+            </div>
+          </Grid.Column>
+          <Grid.Column>
+            <AssignedTo
+              onSelectUserEmail={onSelectUserEmail}
+              assignedUser={assignedUser}
+              users={users}
+              selectedUserId={selectedUserId}
             />
-          </ModalComponent>
-        </Grid.Column>
-      </Grid>
-    </Container>
-  );
-}
+            <ModalComponent
+              trigger={<ButtonComponent {...triggerProps} />}
+              modalTitle="Confirm Action"
+            >
+              <ConfirmAction
+                toggleModal={toggleModal}
+                handleConfirm={this.handleConfirm}
+                buttonState={buttonState}
+                buttonLoading={buttonLoading}
+              />
+            </ModalComponent>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    );
+  }
 }
 
 AssetDescriptionComponent.propTypes = {
