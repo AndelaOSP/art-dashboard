4ae96a9905564d77e038f57cbb8241a5e7baced4
Thuku <thuku.bonnie@gmail.com>
2018-10-15 14:42:31 +0300

    fix(assign button): refactor props refactor asset detail content

--- src/components/AssetDetailComponent.jsx
+++ src/components/AssetDetailComponent.jsx
@@ -42,7 +42,6 @@ export class AssetDetailComponent extends Component {
     } else {
       renderedComponent = (
         <AssetDetailContent
-          {...this.props}
           buttonLoading={this.props.buttonLoading}
           assignedUser={this.state.assignedUser}
           assetDetail={this.props.assetDetail}
