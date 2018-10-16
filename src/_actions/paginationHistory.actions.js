2a138817128faf5b39ec3c6ac8b58e26a9dbd4d9
Fortune Ekeruo <legalmody@gmail.com>
2018-10-16 14:17:10 +0100

    Get rid of redundant loadPaginationHistory action and reducer

--- src/_actions/paginationHistory.actions.js
+++ src/_actions/paginationHistory.actions.js
@@ -1,16 +1,9 @@
 import constants from '../_constants';
 
 const {
-  LOAD_PAGINATION_HISTORY,
   ADD_PAGINATION_HISTORY,
   RESET_PAGINATION_HISTORY
 } = constants;
-
-
-export const loadPaginationHistory = history => ({
-  type: LOAD_PAGINATION_HISTORY,
-  payload: history
-});
 
 export const addPaginationHistory = history => ({
   type: ADD_PAGINATION_HISTORY,
--- src/_reducers/paginationHistory.reducer.js
+++ src/_reducers/paginationHistory.reducer.js
@@ -2,17 +2,12 @@ import constants from '../_constants';
 import initialState from './initialState';
 
 const {
-  LOAD_PAGINATION_HISTORY,
   ADD_PAGINATION_HISTORY,
   RESET_PAGINATION_HISTORY
 } = constants;
 
 export default (state = initialState.paginationHistory, action) => {
   switch (action.type) {
-    case LOAD_PAGINATION_HISTORY:
-      return {
-        ...state
-      };
     case ADD_PAGINATION_HISTORY:
       return {
         ...state,
