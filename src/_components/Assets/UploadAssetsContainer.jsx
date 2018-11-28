aaff075ece3181bdf032a45f0ad2258fbc3a5151
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-11-28 13:15:56 +0300

    ft(upload file): create uploadcontainer and add react-dropzone
    [#160507913]

--- src/_components/Assets/UploadAssetsContainer.jsx
+++ src/_components/Assets/UploadAssetsContainer.jsx
@@ -1,73 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Divider, Grid, Header, Progress } from 'semantic-ui-react';
 
-import NavBarComponent from '../NavBarContainer';
-import StatusMessageComponent from '../../components/common/StatusComponent';
-
-import { createAsset, resetMessage } from '../../_actions/asset.actions';
+import UploadAssetsComponent from '../../components/Assets/UploadAssetsComponent';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class UploadAssets extends React.Component {
   render() {
-    const { success, error } = this.props;
-    const showStatus = success || error;
-
     return (
-      <NavBarComponent>
-        <div className="add-asset">
-          <div id="page-heading-section">
-            <Header as="h1" id="page-headings" floated="left" content="Upload Asset" />
-            <Divider id="assets-divider" />
-          </div>
-
-
-          <Grid centered divided>
-
-            {showStatus && (
-              <Grid.Row>
-                <Grid.Column>
-                  <StatusMessageComponent
-                    message={success || error}
-                    className={success ? 'success-status' : 'error-status'}
-                    reset={this.props.resetMessage}
-                  />
-                </Grid.Column>
-              </Grid.Row>
-            )}
-            <Grid.Column width={10}>
-              <div className="upload-file">
-                <div className="uploader">
-                  <div className="drag-drop">
-                    <span className="span-drag-drop">Drag and Drop</span>
-                  </div>
-                  <button className="choose-file">choose file</button>
-                </div>
-                <div className="progress-bar">
-                  <Progress percent={2} progress />
-                </div>
-              </div>
-            </Grid.Column>
-          </Grid>
-        </div>
-      </NavBarComponent>
+      <UploadAssetsComponent />
     );
   }
 }
 
-UploadAssets.propTypes = {
-  success: PropTypes.string,
-  error: PropTypes.string,
-  resetMessage: PropTypes.func
-};
-
-const mapStateToProps = ({
-  assets
-}) => ({
-  success: assets.success,
-  error: assets.errorMessage
+const mapStateToProps = () => ({
 });
 
-
-export default connect(mapStateToProps, { createAsset, resetMessage })(UploadAssets);
+export default connect(mapStateToProps, {})(UploadAssets);
