87ae59128233ccece315e8bf5f2f6a3a4060a174
Thuku <thuku.bonnie@gmail.com>
2018-11-30 10:42:03 +0300

    feat(upload): add upload assets
     - refactor design

--- src/components/Assets/UploadAssetsComponent.jsx
+++ src/components/Assets/UploadAssetsComponent.jsx
@@ -56,6 +56,14 @@ const UploadAssets = (props) => {
   const { loading, success, error } = props;
   const showStatus = success || error;
 
+  if (loading) {
+    return (
+      <NavBarComponent>
+        <LoaderComponent />
+      </NavBarComponent>
+    );
+  }
+
   return (
     <div className="center-upload">
       <span className="failed-file">
