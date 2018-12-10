d76dbd95ddaf8c20fea611710b3dae07c0786809
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2018-12-10 17:03:44 +0300

    [#160193913] Refactor existing security user logic

---
+++ src/__test__/actions/securityUsers.actions.test.js
@@ -0,0 +1,91 @@
+import expect from 'expect';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { addSecurityUser, loadSecurityUsers } from '../../_actions/securityUsers.actions';
+import constants from '../../_constants';
+import { SecurityUser, securityUsers } from '../../_mock/users';
+
+const {
+  CREATE_SECURITY_USER_REQUEST,
+  CREATE_SECURITY_USER_SUCCESS,
+  CREATE_SECURITY_USER_FAILURE,
+  LOAD_SECURITY_USERS_REQUEST,
+  LOAD_SECURITY_USERS_SUCCESS,
+  LOAD_SECURITY_USERS_FAILURE
+} = constants;
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+let store;
+
+describe('Security users tests', () => {
+  const mock = new MockAdapter(axios);
+  const pageNumber = 1;
+  const limit = 10;
+  const url = '/security-users/';
+  const url2 = `/security-users/?page=${pageNumber}&page_size=${limit}`;
+  store = mockStore({});
+
+  afterEach(() => {
+    store.clearActions();
+  });
+
+  it('should dispatch CREATE_SECURITY_USER_REQUEST when addSecurityUser is called', () => {
+    mock.onPost(url).reply(201, SecurityUser);
+    return store.dispatch(addSecurityUser(SecurityUser)).then(() => {
+      expect(store.getActions()).toContainEqual({
+        type: CREATE_SECURITY_USER_REQUEST
+      });
+    });
+  });
+
+  it('should dispatch CREATE_SECURITY_USER_SUCCESS when addSecurityUser succeeds', () => {
+    mock.onPost(url).reply(201, SecurityUser);
+    return store.dispatch(addSecurityUser(SecurityUser)).then(() => {
+      expect(store.getActions()).toContainEqual({
+        type: CREATE_SECURITY_USER_SUCCESS,
+        payload: SecurityUser
+      });
+    });
+  });
+
+  it('should dispatch CREATE_SECURITY_USER_FAILURE when addSecurityUser fails', () => {
+    mock.onPost(url).reply(401);
+    return store.dispatch(addSecurityUser(SecurityUser)).then(() => {
+      expect(store.getActions()).toContainEqual({
+        type: CREATE_SECURITY_USER_FAILURE,
+        payload: 'Request failed with status code 401'
+      });
+    });
+  });
+
+  it('should dispatch LOAD_SECURITY_USERS_REQUEST when loadSecurityUsers is called', () => {
+    mock.onGet(url2).reply(200);
+    return store.dispatch(loadSecurityUsers(pageNumber, limit)).then(() => {
+      expect(store.getActions()).toContainEqual({
+        type: LOAD_SECURITY_USERS_REQUEST
+      });
+    });
+  });
+
+  it('should dispatch LOAD_SECURITY_USERS_SUCCESS when loadSecurityUsers succeeds', () => {
+    mock.onGet(url2).reply(200, securityUsers);
+    return store.dispatch(loadSecurityUsers(pageNumber, limit)).then(() => {
+      expect(store.getActions()).toContainEqual({
+        type: LOAD_SECURITY_USERS_SUCCESS,
+        payload: securityUsers
+      });
+    });
+  });
+
+  it('should dispatch LOAD_SECURITY_USERS_FAILURE when loadSecurityUsers fails', () => {
+    mock.onGet(url2).reply(401);
+    return store.dispatch(loadSecurityUsers(pageNumber, limit)).then(() => {
+      expect(store.getActions()).toContainEqual({
+        type: LOAD_SECURITY_USERS_FAILURE,
+        payload: 'Request failed with status code 401'
+      });
+    });
+  });
+});
---
+++ src/__test__/reducers/securityUsers.reducer.test.js
@@ -0,0 +1,82 @@
+import expect from 'expect';
+import constants from '../../_constants';
+import securityUsersReducer from '../../_reducers/securityUsers.reducer';
+import { SecurityUser, securityUsers } from '../../_mock/users';
+
+const {
+  CREATE_SECURITY_USER_REQUEST,
+  CREATE_SECURITY_USER_SUCCESS,
+  CREATE_SECURITY_USER_FAILURE,
+  LOAD_SECURITY_USERS_REQUEST,
+  LOAD_SECURITY_USERS_SUCCESS,
+  LOAD_SECURITY_USERS_FAILURE,
+  RESET_STATUS_MESSAGE,
+  SET_USERS_ACTIVE_PAGE
+} = constants;
+
+const initialState = {
+  securityUsers: {
+    activePage: 1,
+    usersList: {},
+    usersCount: 0,
+    errorMessage: '',
+    successMessage: '',
+    isLoading: false
+  }
+};
+
+const action = { payload: {} };
+
+describe('Users Reducer tests', () => {
+  it('should return initial state when there is no action', () => {
+    expect(securityUsersReducer(initialState, action)).toEqual(initialState);
+  });
+
+  it('should handle CREATE_SECURITY_USER_REQUEST', () => {
+    action.type = CREATE_SECURITY_USER_REQUEST;
+    action.payload = SecurityUser;
+    expect(securityUsersReducer(initialState, action).isLoading).toEqual(true);
+  });
+
+  it('should handle CREATE_SECURITY_USER_SUCCESS', () => {
+    action.type = CREATE_SECURITY_USER_SUCCESS;
+    action.payload = SecurityUser;
+    expect(securityUsersReducer(initialState, action).isLoading).toEqual(false);
+  });
+
+  it('should handle CREATE_SECURITY_USER_FAILURE', () => {
+    action.type = CREATE_SECURITY_USER_FAILURE;
+    action.payload = SecurityUser;
+    expect(securityUsersReducer(initialState, action).errorMessage).toEqual(action.payload);
+  });
+
+  it('should handle LOAD_SECURITY_USERS_REQUEST', () => {
+    action.type = LOAD_SECURITY_USERS_REQUEST;
+    action.payload = securityUsers;
+    expect(securityUsersReducer(initialState, action).isLoading).toEqual(true);
+  });
+
+  it('should handle LOAD_SECURITY_USERS_SUCCESS', () => {
+    action.type = LOAD_SECURITY_USERS_SUCCESS;
+    action.payload = securityUsers;
+    expect(securityUsersReducer(initialState, action).usersCount).toEqual(3);
+  });
+
+  it('should handle LOAD_SECURITY_USERS_FAILURE', () => {
+    action.type = LOAD_SECURITY_USERS_FAILURE;
+    action.payload = securityUsers;
+    expect(securityUsersReducer(initialState, action).errorMessage).toEqual(action.payload);
+  });
+
+  it('should handle SET_USERS_ACTIVE_PAGE', () => {
+    action.type = SET_USERS_ACTIVE_PAGE;
+    action.payload = 1;
+    expect(securityUsersReducer(initialState, action).activePage).toEqual(action.payload);
+  });
+
+  it('should handle RESET_STATUS_MESSAGE', () => {
+    action.type = RESET_STATUS_MESSAGE;
+    expect(securityUsersReducer(initialState, action).successMessage).toEqual('');
+    expect(securityUsersReducer(initialState, action).errorMessage).toEqual('');
+  });
+});
