6ef3a8f965af3309e34e3dfd5906c9c2803a807d
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-12-11 10:46:33 +0300

    ft(import-file): fix failing assetfilter tests
    [#160507913]

--- src/__test__/_components/AssetsContainer.test.js
+++ src/__test__/_components/AssetsContainer.test.js
@@ -1,7 +1,5 @@
 import expect from 'expect';
-import { mapStateToProps, createFilterData } from '../../_components/Assets/AssetsContainer';
-import models from '../../_mock/assetModels';
-import types from '../../_mock/assetTypes';
+import { mapStateToProps } from '../../_components/Assets/AssetsContainer';
 
 describe('Renders <Assets />  tests', () => {
   it('calls mapStateToProps', () => {
@@ -40,35 +38,11 @@ describe('Renders <Assets />  tests', () => {
       hasError: false,
       activePage: 1,
       selected: [],
-      filterData: [],
       status: '',
       shouldFetchAssets: true,
       shouldReload: false
     };
 
     expect(mapStateToProps(state, ownProps)).toEqual(expected);
-  });
-
-  it('calls createFilterData', () => {
-    const expected = [
-      {
-        title: 'Asset Types',
-        content: [
-          { id: 0, option: 'Timor-Leste' },
-          { id: 1, option: 'monitoring' },
-          { id: 2, option: 'circuit' }
-        ]
-      },
-      {
-        title: 'Model Numbers',
-        content: [
-          { id: 1, option: 'MC-LifeChat 5' },
-          { id: 2, option: 'Microsoft Lifechat LX-6000' },
-          { id: 3, option: 'Spectre x360' }
-        ]
-      }
-    ];
-
-    expect(createFilterData(types, models)).toEqual(expected);
   });
 });
