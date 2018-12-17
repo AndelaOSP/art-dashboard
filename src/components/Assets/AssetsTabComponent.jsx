bc68b5b569ad5fc3fef78f3f8d048c36192f8c03
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-12-17 07:14:34 +0300

    ft(upload): tabs component [160507913]

--- src/components/Assets/AssetsTabComponent.jsx
+++ src/components/Assets/AssetsTabComponent.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Header, Divider } from 'semantic-ui-react';
+import NavBarComponent from '../../_components/NavBarContainer';
 import AssetsContainer from '../../_components/Assets/AssetsContainer';
 import UploadAssetsContainer from '../../_components/Assets/UploadAssetsContainer';
 import TabsComponent from '../../components/common/TabsComponent';
 
-const AssetsTabComponent = props => (
-  <TabsComponent panes={[
-    {
-      header: 'All Assets',
-      component: <AssetsContainer props={props} />
-    },
-    {
-      header: 'Import Assets',
-      component: <UploadAssetsContainer />
-    }
-  ]}
-  />
-);
+const AssetsTabComponent = (props) => {
+  const contentTitle = props.status ? `${props.status.toLocaleString()} Assets` : 'Assets';
+  return (
+    <NavBarComponent title="Assets">
+      <div className="assets-list">
+        <div id="page-heading-section">
+          <Header as="h1" id="page-headings" floated="left" content={contentTitle} />
+          <Divider id="assets-divider" />
+          <TabsComponent panes={[
+            {
+              header: 'All Assets',
+              component: <AssetsContainer props={props} />
+            },
+            {
+              header: 'Import Assets',
+              component: <UploadAssetsContainer />
+            }
+          ]}
+          />
+        </div>
+      </div>
+    </NavBarComponent>
+  );
+};
+
+AssetsTabComponent.propTypes = {
+  status: PropTypes.string
+};
 
 export default AssetsTabComponent;
--- src/components/AssetsComponent.jsx
+++ src/components/AssetsComponent.jsx
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
-import { Header, Divider } from 'semantic-ui-react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { isEmpty } from 'lodash';
-import NavBarComponent from '../_components/NavBarContainer';
 import AssetsTableContent from './AssetsTableContent';
 import FilterButton from './common/FilterButton';
 import FilterComponent from './common/FilterComponent';
@@ -102,60 +100,52 @@ export default class AssetsComponent extends Component {
     const showPaginator = totalPages > 1;
     const showFilter = !isEmpty(this.props.assetsList[currentAssets] || assets);
 
-    const contentTitle = status ? `${status.toLocaleString()} Assets` : 'Assets';
-
     return (
-      <NavBarComponent title="Assets">
-        <div className="assets-list">
-          <div id="page-heading-section">
-            <Header as="h1" id="page-headings" floated="left" content={contentTitle} />
-            <Divider id="assets-divider" />
-            {showFilter && (
-              <FilterButton
-                activePage={this.props.activePage}
-                limit={this.state.limit}
-                selected={this.props.selected}
-                filterAction={this.props.getAssetsAction}
-                disabled={this.props.isLoading}
-              >
-                <React.Fragment>
-                  <FilterComponent
-                    index={0}
-                    option={this.props.filterData[0]}
-                    selected={this.props.selected}
-                    filterSelection={this.props.filterSelection}
-                  />
-
-                  <FilterComponent
-                    index={1}
-                    option={this.props.filterData[1]}
-                    selected={this.props.selected}
-                    filterSelection={this.props.filterSelection}
-                  />
-                </React.Fragment>
-              </FilterButton>
-            )}
-          </div>
-          <AssetsTableContent
+      <Fragment>
+        {showFilter && (
+          <FilterButton
             activePage={this.props.activePage}
-            assets={this.props.assetsList[currentAssets] || assets}
-            errorMessage={this.props.errorMessage}
-            hasError={this.props.hasError}
+            limit={this.state.limit}
+            selected={this.props.selected}
+            filterAction={this.props.getAssetsAction}
+            disabled={this.props.isLoading}
+          >
+            <React.Fragment>
+              <FilterComponent
+                index={0}
+                option={this.props.filterData[0]}
+                selected={this.props.selected}
+                filterSelection={this.props.filterSelection}
+              />
+
+              <FilterComponent
+                index={1}
+                option={this.props.filterData[1]}
+                selected={this.props.selected}
+                filterSelection={this.props.filterSelection}
+              />
+            </React.Fragment>
+          </FilterButton>
+        )}
+        <AssetsTableContent
+          activePage={this.props.activePage}
+          assets={this.props.assetsList[currentAssets] || assets}
+          errorMessage={this.props.errorMessage}
+          hasError={this.props.hasError}
+          isLoading={this.props.isLoading}
+          status={status}
+        />
+        {showPaginator && (
+          <PaginationComponent
+            activePage={this.props.activePage}
+            handleRowChange={this.handleRowChange}
+            handlePaginationChange={this.handlePaginationChange}
+            limit={this.state.limit}
+            totalPages={totalPages}
             isLoading={this.props.isLoading}
-            status={status}
           />
-          {showPaginator && (
-            <PaginationComponent
-              activePage={this.props.activePage}
-              handleRowChange={this.handleRowChange}
-              handlePaginationChange={this.handlePaginationChange}
-              limit={this.state.limit}
-              totalPages={totalPages}
-              isLoading={this.props.isLoading}
-            />
-          )}
-        </div>
-      </NavBarComponent>
+        )}
+      </Fragment>
     );
   }
 }
