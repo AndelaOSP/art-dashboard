2ebc228e1d526020fae2a0455cb4cf8884c549a8
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-12-18 10:11:27 +0300

    ft(import file): increase coverage
    [#160507913]

---
+++ src/__test__/components/UploadAssetsComponent.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expect from 'expect';
+import UploadAssetsComponent from '../../components/Assets/UploadAssetsComponent';
+
+describe('Renders <AssetsComponent /> correctly', () => {
+  const props = {
+    success: {},
+    error: '',
+    loading: false,
+    downloadedFile: '',
+    downloadFile: jest.fn(),
+    resetUploadAssets: jest.fn(),
+    uploadAssets: jest.fn(),
+    handleDrop: jest.fn(),
+    handleFileDownload: jest.fn(),
+    resetUpload: jest.fn()
+  };
+  const wrapper = shallow(<UploadAssetsComponent {...props} />);
+
+  it('renders dropzone', () => {
+    expect(wrapper.find('.dropzone').length).toBe(1);
+  });
+
+  it('renders success StatusMessage', () => {
+    wrapper.setProps({
+      loading: false,
+      error: 'An error occured'
+    });
+
+    expect(wrapper.find('StatusMessage').hasClass('error-status')).toEqual(true);
+  });
+
+  it('calls the handleDrop function when files are dropped on dropzone', () => {
+    const handleDropSpy = jest.spyOn(
+      wrapper.instance(), 'handleDrop'
+    );
+    const files = [];
+    wrapper.instance().handleDrop(files);
+    expect(handleDropSpy.mock.calls.length).toEqual(1);
+  });
+
+  it('calls the handleFileDownload function when download links are clicked', () => {
+    const handleFileDownloadSpy = jest.spyOn(
+      wrapper.instance(), 'handleFileDownload'
+    );
+    const url = 'test/';
+    wrapper.instance().handleFileDownload(url);
+    expect(handleFileDownloadSpy.mock.calls.length).toEqual(1);
+  });
+
+  it('calls the resetUpload function ', () => {
+    const resetUploadSpy = jest.spyOn(
+      wrapper.instance(), 'resetUpload'
+    );
+    const url = 'test/';
+    wrapper.instance().resetUpload(url);
+    expect(resetUploadSpy.mock.calls.length).toEqual(1);
+  });
+
+  it('should call componentDidUpdate', () => {
+    const componentDidUpdateSpy = jest.spyOn(
+      wrapper.instance(), 'componentDidUpdate'
+    );
+    wrapper.setProps({ downloadedFile: 'blob:testfile/' });
+    expect(componentDidUpdateSpy.mock.calls.length).toEqual(1);
+  });
+});
--- src/components/Assets/UploadAssetsComponent.jsx
+++ src/components/Assets/UploadAssetsComponent.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Progress } from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
-import NavBarComponent from '../../_components/NavBarContainer';
 import { uploadStatus, StatusMessage } from './UploadStatus';
 import errorMessage from './UploadErrorMessages';
 import '../../_css/UploadAssets.css';
@@ -35,47 +34,46 @@ class UploadAssets extends React.Component {
   render() {
     const { loading, success, error } = this.props;
     const showStatus = success || error;
+
     return (
-      <NavBarComponent title="Assets">
-        <div className="center-upload">
-          <span className="failed-file">
-            <a href="# " onClick={() => this.handleFileDownload('sample_import_file/')}>
-              Download the sample file
-            </a>{' '}
-            , fill the columns and upload it.
-          </span>
+      <div className="center-upload">
+        <span className="failed-file">
+          <a href="# " onClick={() => this.handleFileDownload('sample_import_file/')}>
+            Download the sample file
+          </a>{' '}
+          , fill the columns and upload it.
+        </span>
 
-          <Dropzone
-            onDrop={this.handleDrop}
-            accept="text/csv"
-            className="dropzone"
-          >
+        <Dropzone
+          onDrop={this.handleDrop}
+          accept="text/csv"
+          className="dropzone"
+        >
 
-            <div className="progress-handler" onClick={this.resetUpload} onKeyDown={this.resetUpload} role="button" tabIndex={0}>
-              {loading && !showStatus && (
-                <Progress style={{ width: '90%' }} percent={100} active>
-                  Upload in progress...
-                </Progress>
-              )}
+          <div className="progress-handler" onClick={this.resetUpload} onKeyDown={this.resetUpload} role="button" tabIndex={0}>
+            {loading && !showStatus && (
+              <Progress style={{ width: '90%' }} percent={100} active>
+                Upload in progress...
+              </Progress>
+            )}
 
-              {showStatus && (
-                <StatusMessage
-                  message={uploadStatus(success, error)}
-                  className={success.fail || error ? 'error-status' : 'success-status'}
-                  reset={this.resetUpload}
-                />
-              )}
+            {showStatus && (
+              <StatusMessage
+                message={uploadStatus(success, error)}
+                className={success.fail || error ? 'error-status' : 'success-status'}
+                reset={this.resetUpload}
+              />
+            )}
 
-              {!showStatus && !loading && (
-                <p>Drag and drop the file here, or click to select the file to upload.</p>
-              )}
-            </div>
+            {!showStatus && !loading && (
+              <p>Drag and drop the file here, or click to select the file to upload.</p>
+            )}
+          </div>
 
-          </Dropzone>
+        </Dropzone>
 
-          {errorMessage(error, success, this.handleFileDownload)}
-        </div>
-      </NavBarComponent>
+        {errorMessage(error, success, this.handleFileDownload)}
+      </div>
     );
   }
 }
