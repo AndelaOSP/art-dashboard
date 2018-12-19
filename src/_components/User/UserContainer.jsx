154bd0d278eb3a896d04aaa8c58cc31d64983b00
Ewanjiru <eunicewanjirunjeri@gmail.com>
2018-12-19 14:19:30 +0300

    ft(import): final commit
    [FInished #160507913]

--- src/_components/User/UserContainer.jsx
+++ src/_components/User/UserContainer.jsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { loadAllFilterValues } from '../../_actions/allFilterValues.actions';
 import { loadUsers, setActivePage, resetUsers, loading, resetMessage } from '../../_actions/users.actions';
-import UserFilter from '../../components/User/UserFilter';
+import User from '../../components/User/UsersComponent';
 
 export const mapStateToProps = ({ usersList, selected }) => {
   const {
@@ -33,4 +33,4 @@ export default connect(mapStateToProps, {
   loading,
   loadAllFilterValues,
   resetMessage
-})(UserFilter);
+})(User);
