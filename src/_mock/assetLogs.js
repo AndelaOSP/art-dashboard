4e504605a9cc710d4e546bcdc70384c4b683ff33
andela-fojuri <fisayomi.ojuri@andela.com>
2019-03-21 02:09:38 +0100

    feat: Update Tests

--- src/_mock/assetLogs.js
+++ src/_mock/assetLogs.js
@@ -3,16 +3,49 @@ export default [
   {
     id: 1,
     asset: 'Asset Fake A',
-    log_type: 'Checkin'
+    log_type: 'Checkin',
+    created_at: '2019-02-27T10:54:43.489280Z'
   },
   {
     id: 2,
-    asset: 'Asset Fake B',
-    log_type: 'Checkin'
+    asset: 'Asset Fake A',
+    log_type: 'Checkout',
+    created_at: '2019-02-27T10:54:43.489280Z'
   },
   {
     id: 3,
+    asset: 'Asset Fake B',
+    log_type: 'Checkin',
+    created_at: '2019-02-27T10:54:43.489280Z'
+  },
+  {
+    id: 4,
+    asset: 'Asset Fake B',
+    log_type: 'Checkout',
+    created_at: '2019-02-27T10:54:43.489280Z'
+  },
+  {
+    id: 5,
     asset: 'Asset Fake C',
-    log_type: 'Checkout'
+    log_type: 'Checkin',
+    created_at: '2019-02-27T10:54:43.489280Z'
+  },
+  {
+    id: 6,
+    asset: 'Asset Fake C',
+    log_type: 'Checkout',
+    created_at: '2019-03-27T10:54:43.489280Z'
+  },
+  {
+    id: 7,
+    asset: 'Asset Fake C',
+    log_type: 'Checkin',
+    created_at: '2019-03-27T10:54:47.489280Z'
+  },
+  {
+    id: 8,
+    asset: 'Asset Fake D',
+    log_type: 'Checkout',
+    created_at: '2019-03-27T10:54:48.489280Z'
   }
 ];
