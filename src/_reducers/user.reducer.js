f7c58de2923cca2c4c01555a180ce56bf3d1e7e9
Onyekachi Ezeoke <ezeokeonyekachi@gmail.com>
2019-05-14 09:01:08 +0100

    [#165785568] implement feature to enable super user to grant access on which user to be staff - create EditableUserDetailsComponent component - add actions and reducers - add test for EditableUserDetailsComponent component - add test for actions - add test for reducers - implement feedback on pr review [finishes #165785568]

--- src/_reducers/user.reducer.js
+++ src/_reducers/user.reducer.js
@@ -1,7 +1,13 @@
 import constants from '../_constants';
 import initialState from './initialState';
 
-const { LOADING_USER, LOAD_USER_SUCCESS, LOAD_USER_FAILURE } = constants;
+const {
+  LOADING_USER,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAILURE,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILURE
+} = constants;
 
 export default (state = initialState.user, action) => {
   switch (action.type) {
@@ -19,6 +25,20 @@ export default (state = initialState.user, action) => {
       };
 
     case LOAD_USER_FAILURE:
+      return {
+        isLoading: action.isLoading,
+        hasError: true,
+        errorMessage: action.payload
+      };
+
+    case UPDATE_USER_SUCCESS:
+      return {
+        ...state,
+        userDetail: action.payload,
+        isLoading: action.isLoading
+      };
+
+    case UPDATE_USER_FAILURE:
       return {
         isLoading: action.isLoading,
         hasError: true,
